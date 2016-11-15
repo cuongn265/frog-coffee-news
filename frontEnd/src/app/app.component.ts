a4846f3fa8ebac3cfe1c1589f506dd37a1bd90dd
CSEugene <cuongnm265@gmail.com>
2016-11-15 14:56:46 +0700

    Toolbar and Sidenav layout

--- frontEnd/src/app/app.component.ts
+++ frontEnd/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  links = ['Mobile', 'Web', 'Hibrid'];
 }
--- frontEnd/src/main.ts
+++ frontEnd/src/main.ts
@@ -1,5 +1,5 @@
 import './polyfills.ts';
-
+import * as jQuery from 'jquery';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
@@ -9,4 +9,4 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+jQuery(() => platformBrowserDynamic().bootstrapModule(AppModule));
