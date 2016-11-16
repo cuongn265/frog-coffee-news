1368c8c047e65143690890e61408f97da2decb9a
CSEugene <cuongnm265@gmail.com>
2016-11-16 16:51:11 +0700

    1st integrate commit

---
+++ frontEnd/src/app/category.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Category } from './category';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class CategoryService {
+
+  private categoryUrl: string = 'http://localhost:3000/api/category';
+
+  constructor(private http: Http) { }
+
+  getCategories(): Promise<Category[]> {
+    return this.http.get(this.categoryUrl)
+      .toPromise()
+      .then(response => response.json())
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error);
+    return Promise.reject(error.message || error);
+  }
+}
