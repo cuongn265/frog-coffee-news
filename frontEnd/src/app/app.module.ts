6866eaeda830e4d40eceb68b34667c04cd2feaa5
CSEugene <cuongnm265@gmail.com>
2016-11-18 11:50:33 +0700

    Route with category

--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MaterialModule } from '@angular/material';
 import { CollapseDirective } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
+import { AppRoutes } from './app.routing';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { ArticleComponent } from './article/article.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule.forRoot()
+    MaterialModule.forRoot(),
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
