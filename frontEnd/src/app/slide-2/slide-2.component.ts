56b2cd4d974ab162ce463e8e4eb1cc322fe3a2d3
CSEugene <cuongnm265@gmail.com>
2017-01-08 13:41:32 +0700

    fix slide doesn't work when route change

--- frontEnd/src/app/slide-2/slide-2.component.ts
+++ frontEnd/src/app/slide-2/slide-2.component.ts
@@ -1,49 +1,30 @@
 import { ArticleService } from './../article/article.service';
 import { Article } from './../article/article';
 import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
-import { Slide } from './slide2';
-import { SlideService } from './slide2.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 declare var $: any;
 
 @Component({
   selector: 'app-slide-2',
   templateUrl: './slide-2.component.html',
   styleUrls: ['./slide-2.component.scss'],
-  providers: [SlideService, ArticleService]
+  providers: [ArticleService]
 })
 export class Slide2Component implements OnInit, AfterViewInit {
-  slideList: Slide[];
-  slideAllList: Slide[];
   sub: any;
   articlesList: Article[];
   publishedArticles: Article[];
   categoryName: string;
-  constructor(private slideService: SlideService, private articleService: ArticleService,
-    private el: ElementRef, private route: ActivatedRoute) { }
+  constructor(private articleService: ArticleService,
+    private el: ElementRef,
+    private route: ActivatedRoute,
+    private router: Router) {
+  }
 
   ngAfterViewInit() {
-    console.log('after view init');
     $(this.el.nativeElement).ready(function () {
       $('#myCarousel').carousel({
-        interval: 2000
-      });
-      let clickEvent = false;
-      $('#myCarousel').on('click', '.nav a', function () {
-        clickEvent = true;
-        $('.nav li').removeClass('active');
-        $(this).parent().addClass('active');
-      }).on('slid.bs.carousel', function (e) {
-        if (!clickEvent) {
-          let count = $('.nav').children().length - 1;
-          let current = $('.nav li.active');
-          current.removeClass('active').next().addClass('active');
-          let id = parseInt(current.data('slide-to'));
-          if (count === id) {
-            $('.nav li').first().addClass('active');
-          }
-        }
-        clickEvent = false;
+        interval: 3000
       });
     });
   }
@@ -61,6 +42,11 @@ export class Slide2Component implements OnInit, AfterViewInit {
               this.publishedArticles.push(article);
             }
           }, this);
+          $(this.el.nativeElement).ready(function () {
+            $('#myCarousel').carousel({
+              interval: 3000
+            });
+          });
         }
       );
     });
--- frontEnd/src/app/slide-2/slide2.ts
+++
@@ -1,8 +0,0 @@
-export class Slide{
-    private idProduct:number;
-    private productName:string;
-    private URL_Picture:string;
-    private Unit_Price:number;
-    private Discount:number;
-    private CateID:number;
-}
