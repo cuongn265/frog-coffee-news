79b51275cdb5794e043452c379546f2b261a8fd3
CSEugene <cuongnm265@gmail.com>
2017-01-08 19:48:32 +0700

    fix callback url and social login

--- frontEnd/src/app/auth.service.ts
+++ frontEnd/src/app/auth.service.ts
@@ -14,8 +14,37 @@ export class AuthService {
       primaryColor: '#673ab7'
     },
     languageDictionary: {
-      title: 'Frog Coffee News'
-    }
+      title: 'Frog Coffee News',
+      error: {
+        login: {
+          'lock.invalid_email_password': 'Custom message about invalid credentials',
+          'lock.network': 'Custom message indicating a network error and suggesting the user check connection',
+          'lock.unauthorized': 'Custom message about a failure of permissions',
+          'too_many_attempts': 'Custom message indicating the user has failed to login too many times.'
+        },
+        signUp: {
+          'invalid_password': 'Password is invalid.',
+          'password_no_user_info_error': 'Password is based on user information.',
+          'password_strength_error': 'Password is too weak.',
+          'user_exists': 'The user already exists.',
+          'username_exists': 'The username already exists.'
+        }
+      },
+    },
+    auth: { redirect: false },
+    socialButtonStyle: 'small',
+    additionalSignUpFields: [{
+      name: 'firstName',
+      placeholder: 'Enter your first name'
+    },
+    {
+      name: 'lastName',
+      placeholder: 'Enter your last name'
+    },
+    {
+      name: 'phone',
+      placeholder: 'Enter your phone number'
+    }]
   };
   lock = new Auth0Lock('8VZeo0lbIPEz3OCGSVuC4AdWvKZBD0k9', 'cuongnm265.au.auth0.com', this.options);
   userProfile: User;
@@ -35,6 +64,15 @@ export class AuthService {
         this.userProfile = profile;
       });
     });
+
+    this.lock.on('authorization_error', function (error) {
+      this.lock.show({
+        flashMessage: {
+          type: 'error',
+          text: error.error_description
+        }
+      });
+    }, this);
   }
 
   public login() {
