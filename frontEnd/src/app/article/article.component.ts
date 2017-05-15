0bc83e0fe574dcd0fc51257798558cc63a9b45e0
cuongnm265 <cuongnm265@gmail.com>
2017-05-15 10:44:32 +0700

    Inifinit Scroll (need to increse number of article when there're more)

--- frontEnd/src/app/article/article.component.ts
+++ frontEnd/src/app/article/article.component.ts
@@ -19,6 +19,12 @@ export class ArticleComponent implements OnInit, OnDestroy {
   articlesList: Article[];
   categoryName: string;
   publishedArticles: Article[];
+  array = [];
+  infiniteArticles: Article[] = [];
+  sum = 6;
+  throttle = 400;
+  scrollDistance = 2;
+  articleIndex = 0;
   private sub: any;
   constructor(private articleService: ArticleService, private route: ActivatedRoute) {
   }
@@ -37,6 +43,7 @@ export class ArticleComponent implements OnInit, OnDestroy {
               this.publishedArticles.push(article);
             }
           }, this);
+          this.addItems(0, this.sum);
         }
       );
     });
@@ -44,5 +51,34 @@ export class ArticleComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.sub.unsubscribe();
+  }
+
+  addItems(startIndex, endIndex) {
+    let i = this.articleIndex;
+    for (; i < this.sum; ++i) {
+      console.log("----------: " + i);
+      console.log(this.publishedArticles[i]);
+      console.log('----------');
+      this.articleIndex++;
+      this.infiniteArticles.push(this.publishedArticles[i]);
+    }
+    console.log(this.infiniteArticles)
+  }
+  onScrollDown() {
+    console.log('scrolled!!');
+
+    // add another 20 items
+    const start = this.sum;
+    console.log('start: ' + start);
+    this.sum += 6;
+    console.log('sum: ' + this.sum);
+    if (this.sum >= this.publishedArticles.length) {
+      this.sum = this.publishedArticles.length
+    }
+    this.addItems(start, this.sum);
+  }
+
+  onScroll() {
+    console.log('scrolled!!')
   }
 }
