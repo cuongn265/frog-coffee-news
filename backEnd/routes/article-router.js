94ac79e7bdff600ab0b6829b2ac45037ded82396
Thang Le Quoc <silverhair.guy@gmail.com>
2017-06-06 01:54:53 +0700

    adjust response on posting article with no tag

--- backEnd/routes/article-router.js
+++ backEnd/routes/article-router.js
@@ -69,6 +69,9 @@ router.route('/')
                         res.status(400).send(err);
                     })
                 }
+                else{
+                    res.status(201).send();
+                }
             }
         });
     });
